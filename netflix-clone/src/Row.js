492fc6721150571f900ec26412ba352cd08a5ffa
KISHOTH KUMAR D <kishothkumarcse@gmail.com>
2023-06-18 22:55:48 +0530

    init

---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,37 @@
+import axios from './axios';
+import React, { useEffect, useState } from 'react';
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({title, fetchURL, isLargeRow}) {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const request = await axios.get(fetchURL);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    },[fetchURL]);
+
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className='row__posters'>
+                {movies.map(movie => (
+                    <img 
+                        key={movie.id}
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                        alt={movie.name}>
+                    </img>
+                ))}
+            </div> 
+        </div>
+  )
+}
+
+export default Row
